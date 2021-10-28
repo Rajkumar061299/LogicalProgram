3272818e196a54a5a6e09dd94365f61ea3fc3096
Rajkumar <rajkumar67272@gmail.com>
2021-10-28 19:47:00 +0530

    solve with another way

--- src/program/Demo1.java
+++ src/program/Demo1.java
@@ -2,6 +2,9 @@ package program;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Demo1 {
@@ -20,12 +23,30 @@ public class Demo1 {
 
 	}
 
+	public static String solution1(String s) {
+
+		Map<Integer, String> map = Arrays.stream(s.split(" ")).collect(
+
+				Collectors.toMap(
+
+						word -> Integer.parseInt(word.replaceAll("[^0-9]", "")), word1 -> word1
+
+						, (oldValue, newValue) -> oldValue
+
+						, TreeMap::new));
+		return 
+		map.values().stream().map(word -> word.replaceAll("[^a-zA-Z | \s]", "")).collect(Collectors.joining(" "));
+
+
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println(solution("a2re rajku4mar 1how you3"));
-		
-		// a2re rajku4mar 1how you3 --> how are you rajkumar
 
+		System.out.println(solution1("a2re rajku4mar 1how you3"));
+
+		// a2re rajku4mar 1how you3 --> how are you rajkumar
 
 	}
 
